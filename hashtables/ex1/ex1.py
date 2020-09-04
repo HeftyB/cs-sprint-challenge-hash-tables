aab3aade5bd12fd46f68bf548287a214ec66336e
Andrew Shields <andrewshields@Andrews-iMac.attlocal.net>
2020-09-04 13:23:19 -0400

    Completed all 5 questions

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,18 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    wldict = {}
+    
+    for index, weight in enumerate(weights):
+        wldict[limit - weight] = index
+    
+    for index, i in enumerate(weights):
+        if i in wldict:
+            if index > wldict[i]:
+                ret = (index, wldict[i])
+                return ret
+            else:
+                ret = (wldict[i], index)
+                return ret
 
     return None
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,21 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    tickdict = {}
+
+    for i in tickets:
+        if i.source == "NONE":
+            tickdict["starts"] = i
+        else:
+            tickdict[i.source] = i
+
+    route = [None] * length
+
+    route[0] = tickdict["starts"].destination
+
+    for index, i in enumerate(route):
+        if index + 1 == length:
+            break
+        route[index + 1] = tickdict[i].destination
 
     return route
